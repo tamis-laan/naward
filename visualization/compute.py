64485713855d59cd7a8c0512e678013a3ec894ad
Your Name <you@example.com>
2014-07-14 14:51:49 +0200

    Added country stats

--- visualization/compute.py
+++ visualization/compute.py
@@ -2,12 +2,20 @@ import json
 from collections import defaultdict
 
 country_list = defaultdict(dict);
+min_balance   =  9999999999999
+max_balance   = -9999999999999
+total_balance = 0;
 
-file = open('./swag.json')
+file = open('./test_file.json')
 bitcoins = json.load(file)
 
 for bitcoin in bitcoins:
 	for country in bitcoins[bitcoin]["countries"]:
+		total_balance += bitcoins[bitcoin]["balance"]
+		if bitcoins[bitcoin]["balance"]<min_balance:
+			min_balance = bitcoins[bitcoin]["balance"]
+		if bitcoins[bitcoin]["balance"]>max_balance:
+			max_balance = bitcoins[bitcoin]["balance"]
 		try:
 			country_list[country]["balance"] += bitcoins[bitcoin]["balance"]
 			country_list[country]["lang"] += bitcoins[bitcoin]["langs"]
@@ -15,9 +23,10 @@ for bitcoin in bitcoins:
 			country_list[country]["balance"] = bitcoins[bitcoin]["balance"]
 			country_list[country]["lang"] = bitcoins[bitcoin]["langs"]
 		
-for country in country_list:
-	print country , country_list[country]
+stats = {"min_balance":min_balance,"max_balance":0,"total_balance":total_balance}
+out = {"stats":stats,"country list":country_list}
 
+print out
 
 with open('country_index.json', 'wb') as fp:
-	json.dump(country_list, fp)
+	json.dump(out, fp)
