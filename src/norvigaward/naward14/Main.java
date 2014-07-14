4f4633e1065c55ce40f53c3a8d59081c6a54979f
Pieter Hameete <phameete@gmail.com>
2014-07-14 13:02:56 +0200

    remove dep nekohtml

--- src/norvigaward/naward14/Main.java
+++ src/norvigaward/naward14/Main.java
@@ -7,10 +7,6 @@ import norvigaward.naward14.hadoop.warc.BitcoinAddresses;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.ToolRunner;
 
-/**
- * Hello world!
- *
- */
 public class Main 
 {
     public static void main( String[] args )
